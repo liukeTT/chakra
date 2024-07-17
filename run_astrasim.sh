9eb9dd09c2989e086e773ae858386dcb58a5792a
Liu Ke <liuke@tenstorrent.com>
2024-07-16 19:07:05 -0700

    chakra launched by llm-perf-model

--- run_astrasim.sh
+++ run_astrasim.sh
@@ -2,30 +2,22 @@
 
 ASTRASIM_DIR="/workspace/astra-sim"
 CHAKRA_DIR="/workspace/chakra"
+LLM_DIR="/workspace/llm-perf-model/gpt_inference"
 
 # build astrasim
 ${ASTRASIM_DIR}/build/astra_analytical/build.sh
 BINARY="${ASTRASIM_DIR}/build/astra_analytical/build/bin/AstraSim_Analytical_Congestion_Unaware"
 #BINARY="${ASTRASIM_DIR}/build/astra_analytical/build/bin/AstraSim_Analytical_Congestion_Aware"
 
-for comm in "allreduce" "alltoall" "allgather" "reducescatter"; do
-for comm_size in 1024 1048576 1073741824; do
-for TOPO in "Switch" "Ring" "FullyConnected"; do
-    # chakra: generated et
-    WORKLOAD="${CHAKRA_DIR}/et/${comm}/${comm_size}/node"
+WORKLOAD="${CHAKRA_DIR}/et/node"
 
-    # astrasim: system config
-    SYSTEM="${ASTRASIM_DIR}/inputs/system/${TOPO}.json"
-    NETWORK="${ASTRASIM_DIR}/inputs/network/analytical/${TOPO}.yml"
-    MEMORY="${ASTRASIM_DIR}/inputs/remote_memory/analytical/no_memory_expansion.json"
+NETWORK="${LLM_DIR}/$1"
+SYSTEM="${LLM_DIR}/$2"
+MEMORY="${LLM_DIR}/$3"
 
-    # run astrasim
-    ${BINARY} \
-        --workload-configuration="${WORKLOAD}" \
-        --system-configuration="${SYSTEM}" \
-        --network-configuration="${NETWORK}" \
-        --remote-memory-configuration="${MEMORY}" \
-        >> ./et/${comm}/${comm_size}/astrasim.${TOPO}
-done
-done
-done
+# run astrasim
+${BINARY} \
+    --workload-configuration="${WORKLOAD}" \
+    --system-configuration="${SYSTEM}" \
+    --network-configuration="${NETWORK}" \
+    --remote-memory-configuration="${MEMORY}"
--- run_generator.sh
+++ run_generator.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # if not install chakra package, uncomment following
-python3 -m pip install . --user
+# cd /workspace/chakra/
+# python3 -m pip install . --user
 
 # clean et folder
 rm -fr ./et/*
 
 # generate et
-for comm in "allreduce" "alltoall" "allgather" "reducescatter"; do
-for comm_size in 1024 1048576 1073741824; do
-    output_dir="./et/${comm}/${comm_size}"
-    mkdir -p ${output_dir}
+comm=$1
+num_npus=$2
+num_dims=$3
+comm_size=$4
 
-    python3 -m et_generator.et_comms --comm ${comm} --num_npus 64 --num_dims 1 --comm_size ${comm_size} --output_dir ${output_dir}
-done
-done
+output_dir="/workspace/chakra/et"
+mkdir -p ${output_dir}
+python3 -m et_generator.et_comms --comm ${comm} --num_npus ${num_npus} --num_dims ${num_dims} --comm_size ${comm_size} --output_dir ${output_dir}
